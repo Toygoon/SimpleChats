8a2d27f99075fe984b50f29b1e44d3bec74026b6
Toygoon <toygoon@gmail.com>
2022-06-15 11:13:57 +0900

    comment

--- src/common.h
+++ src/common.h
@@ -27,23 +27,37 @@
 #include <sys/socket.h>
 #include <unistd.h>
 
+/* MemberInfo : 멤버 정보를 저장하는 구조체 */
 typedef struct _MemberInfo {
+    // 소켓 번호
     int socket;
+    // 멤버 이름
     char name[NAME_SIZE];
+    // 비활성화 여부
     bool isDisabled;
+    // 방에 입장했는지 여부
     bool enteredRoom;
+    // LinkedList처럼 다음 노드를 가짐
     struct MemberInfo* next;
 } MemberInfo;
 
+/* SocketInfo : 방에서 소켓을 저장하기 위한 구조체 */
 typedef struct _SocketInfo {
+    // 소켓 번호
     int socket;
+    // 비활성화 여부
     bool isDisabled;
+    // LinkedList처럼 다음 노드를 가짐
     struct SocketInfo* next;
 } SocketInfo;
 
+/* RoomInfo : 방 정보를 저장하기 위한 구조체 */
 typedef struct _RoomInfo {
+    // 방 이름
     char name[NAME_SIZE];
+    // 소켓 정보를 담는 LinkedList
     struct SocketInfo* roomSocketList;
+    // 방 정보를 담는 LinkedList
     struct RoomInfo* next;
 } RoomInfo;
 
